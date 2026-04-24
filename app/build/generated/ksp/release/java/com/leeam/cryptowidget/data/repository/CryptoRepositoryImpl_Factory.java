600ea09d482ee671fbc134f51413e955524a6114
Leeam Sweeney <electream.13@gmail.com>
2026-04-24 10:33:22 -0700

    auto-commit: 2026-04-24 10:33 (session-end)

--- app/build/generated/ksp/release/java/com/leeam/cryptowidget/data/repository/CryptoRepositoryImpl_Factory.java
+++ app/build/generated/ksp/release/java/com/leeam/cryptowidget/data/repository/CryptoRepositoryImpl_Factory.java
@@ -1,6 +1,10 @@
 package com.leeam.cryptowidget.data.repository;
 
+import com.leeam.cryptowidget.data.remote.BitcoinService;
+import com.leeam.cryptowidget.data.remote.EthereumService;
+import com.leeam.cryptowidget.data.remote.GenericRestService;
 import com.leeam.cryptowidget.data.remote.KrakenService;
+import com.leeam.cryptowidget.data.remote.SolanaService;
 import com.leeam.cryptowidget.data.remote.XrplService;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.Factory;
@@ -30,24 +34,49 @@ public final class CryptoRepositoryImpl_Factory implements Factory<CryptoReposit
 
   private final Provider<XrplService> xrplServiceProvider;
 
+  private final Provider<BitcoinService> bitcoinServiceProvider;
+
+  private final Provider<EthereumService> ethereumServiceProvider;
+
+  private final Provider<SolanaService> solanaServiceProvider;
+
+  private final Provider<GenericRestService> genericRestServiceProvider;
+
+  private final Provider<CoinRepository> coinRepositoryProvider;
+
   private CryptoRepositoryImpl_Factory(Provider<KrakenService> krakenServiceProvider,
-      Provider<XrplService> xrplServiceProvider) {
+      Provider<XrplService> xrplServiceProvider, Provider<BitcoinService> bitcoinServiceProvider,
+      Provider<EthereumService> ethereumServiceProvider,
+      Provider<SolanaService> solanaServiceProvider,
+      Provider<GenericRestService> genericRestServiceProvider,
+      Provider<CoinRepository> coinRepositoryProvider) {
     this.krakenServiceProvider = krakenServiceProvider;
     this.xrplServiceProvider = xrplServiceProvider;
+    this.bitcoinServiceProvider = bitcoinServiceProvider;
+    this.ethereumServiceProvider = ethereumServiceProvider;
+    this.solanaServiceProvider = solanaServiceProvider;
+    this.genericRestServiceProvider = genericRestServiceProvider;
+    this.coinRepositoryProvider = coinRepositoryProvider;
   }
 
   @Override
   public CryptoRepositoryImpl get() {
-    return newInstance(krakenServiceProvider.get(), xrplServiceProvider.get());
+    return newInstance(krakenServiceProvider.get(), xrplServiceProvider.get(), bitcoinServiceProvider.get(), ethereumServiceProvider.get(), solanaServiceProvider.get(), genericRestServiceProvider.get(), coinRepositoryProvider.get());
   }
 
   public static CryptoRepositoryImpl_Factory create(Provider<KrakenService> krakenServiceProvider,
-      Provider<XrplService> xrplServiceProvider) {
-    return new CryptoRepositoryImpl_Factory(krakenServiceProvider, xrplServiceProvider);
+      Provider<XrplService> xrplServiceProvider, Provider<BitcoinService> bitcoinServiceProvider,
+      Provider<EthereumService> ethereumServiceProvider,
+      Provider<SolanaService> solanaServiceProvider,
+      Provider<GenericRestService> genericRestServiceProvider,
+      Provider<CoinRepository> coinRepositoryProvider) {
+    return new CryptoRepositoryImpl_Factory(krakenServiceProvider, xrplServiceProvider, bitcoinServiceProvider, ethereumServiceProvider, solanaServiceProvider, genericRestServiceProvider, coinRepositoryProvider);
   }
 
   public static CryptoRepositoryImpl newInstance(KrakenService krakenService,
-      XrplService xrplService) {
-    return new CryptoRepositoryImpl(krakenService, xrplService);
+      XrplService xrplService, BitcoinService bitcoinService, EthereumService ethereumService,
+      SolanaService solanaService, GenericRestService genericRestService,
+      CoinRepository coinRepository) {
+    return new CryptoRepositoryImpl(krakenService, xrplService, bitcoinService, ethereumService, solanaService, genericRestService, coinRepository);
   }
 }
